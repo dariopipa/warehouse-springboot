a3e0a497f473bd2f438bad36c00a71ca444e817b
user3306 <Dariopipa733@gmail.com>
2025-07-27 19:03:40 +0200

    Full email integration && refactor the stockalertservice to be more readbale and extract methods from the main public method

--- src/main/java/io/github/dariopipa/warehouse/services/EmailServiceImpl.java
+++ src/main/java/io/github/dariopipa/warehouse/services/EmailServiceImpl.java
@@ -1,16 +1,47 @@
 package io.github.dariopipa.warehouse.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import io.github.dariopipa.warehouse.dtos.requests.SendEmailDTO;
 import io.github.dariopipa.warehouse.services.interfaces.EmailService;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
-	@Override
-	public void sendEmail() {
-		// TODO Auto-generated method stub
+	private final Logger logger = LoggerFactory
+			.getLogger(EmailServiceImpl.class);
 
+	private final JavaMailSender javaMailSender;
+	public EmailServiceImpl(JavaMailSender javaMailSender) {
+		this.javaMailSender = javaMailSender;
 	}
 
+	// TODO: MAKE THE SENDING OF EMAILS TO ACTUALLY ALLOW BULK SENDING BY
+	// CHANGING THE HELPER
+	@Override
+	public void sendEmail(SendEmailDTO emailDTO) {
+
+		try {
+			MimeMessage message = javaMailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+			helper.setFrom(
+					new InternetAddress(emailDTO.getFrom(), "Warehouse App"));
+			helper.setTo(emailDTO.getTo());
+			helper.setSubject(emailDTO.getSubject());
+			helper.setText(emailDTO.getBody(), true);
+
+			javaMailSender.send(message);
+
+		} catch (Exception e) {
+			logger.warn("Email failed to send to {}: {}", emailDTO.getTo(),
+					e.getMessage());
+		}
+	}
 }
--- src/main/java/io/github/dariopipa/warehouse/services/interfaces/EmailService.java
+++ src/main/java/io/github/dariopipa/warehouse/services/interfaces/EmailService.java
@@ -1,5 +1,7 @@
 package io.github.dariopipa.warehouse.services.interfaces;
 
+import io.github.dariopipa.warehouse.dtos.requests.SendEmailDTO;
+
 public interface EmailService {
-	void sendEmail();
+	void sendEmail(SendEmailDTO emailDTO);
 }
