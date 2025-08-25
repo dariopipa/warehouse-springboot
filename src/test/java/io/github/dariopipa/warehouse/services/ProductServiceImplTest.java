bda5a0248059ac5872a55ede4b6200ecd59db12e
user3306 <Dariopipa733@gmail.com>
2025-08-25 18:31:24 +0200

    source formatting and import formatting

--- src/test/java/io/github/dariopipa/warehouse/services/ProductServiceImplTest.java
+++ src/test/java/io/github/dariopipa/warehouse/services/ProductServiceImplTest.java
@@ -1,11 +1,11 @@
 package io.github.dariopipa.warehouse.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import java.util.Optional;
